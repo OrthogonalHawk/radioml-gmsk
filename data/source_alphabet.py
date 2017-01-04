85988cd234d875f34bd0a914058ec78af3279790
Orthogonal Hawk <OrthogonalHawk@gmail.com>
2017-01-03 20:55:54 -0600

    Modified random dataset functionality so that the  master source file is split into pieces and each  piece is used in order. helps to ensure that more  of the input file is used during dataset generation.

--- data/source_alphabet.py
+++ data/source_alphabet.py
@@ -16,34 +16,64 @@
 
 from gnuradio import gr, blocks
 import numpy as np
+import os
+import shutil
+from subprocess import call
 import sys
 
 class source_alphabet(gr.hier_block2):
+
+    MASTER_SOURCE_FILE = "source_material/gutenberg_shakespeare.txt"
+    SPLIT_FILE_DIR = "/tmp/source_alphabet/"
+    SOURCE_FILES = []
+    CUR_SOURCE_FILE_IDX = 0
+    SOURCE_SPLIT = False
+
     def __init__(self, dtype="discrete", limit=10000, unpack_bytes=False, randomize=False):
+
+        if self.SOURCE_SPLIT == False:
+            if os.path.exists(source_alphabet.SPLIT_FILE_DIR):
+                shutil.rmtree(source_alphabet.SPLIT_FILE_DIR)
+
+            # make the destination directory
+            os.makedirs(source_alphabet.SPLIT_FILE_DIR)
+
+            # split the source file into smaller chunks
+            call("split --bytes 500K %s %ssource_piece_" % (source_alphabet.MASTER_SOURCE_FILE, source_alphabet.SPLIT_FILE_DIR), shell=True)
+
+            # get a list of the resulting files
+            source_alphabet.SOURCE_FILES = os.listdir(source_alphabet.SPLIT_FILE_DIR)
+
+            # this only needs to be done once; set the flag
+            source_alphabet.SOURCE_SPLIT = True
+
         if(dtype == "discrete"):
             gr.hier_block2.__init__(self, "source_alphabet",
                 gr.io_signature(0,0,0),
                 gr.io_signature(1,1,gr.sizeof_char))
 
-            self.src = blocks.file_source(gr.sizeof_char, "source_material/gutenberg_shakespeare.txt")
+            # select either the master source file or one of the split files
+            if randomize:
+                file_to_use = "%s%s" % (source_alphabet.SPLIT_FILE_DIR, source_alphabet.SOURCE_FILES[source_alphabet.CUR_SOURCE_FILE_IDX])
+                print "Using %s" % (file_to_use)
+                self.src = blocks.file_source(gr.sizeof_char, "%s" % (file_to_use))
 
+                # on each use update the idx, but make sure it does not
+                #  increment too far
+                source_alphabet.CUR_SOURCE_FILE_IDX += 1
+                source_alphabet.CUR_SOURCE_FILE_IDX % len(source_alphabet.SOURCE_FILES)
+            else:
+                self.src = blocks.file_source(gr.sizeof_char, source_alphabet.MASTER_SOURCE_FILE)
+
+            # optionally unpack the bytes
             if unpack_bytes:
                 self.convert = blocks.packed_to_unpacked_bb(1, gr.GR_LSB_FIRST)
             else:
                 self.convert = blocks.packed_to_unpacked_bb(8, gr.GR_LSB_FIRST)
+
             self.limit = blocks.head(gr.sizeof_char, limit)
             self.connect(self.src,self.convert)
             last = self.convert
-
-            # whiten our sequence with a random block scrambler (optionally)
-            if(randomize):
-                rand_len = 256
-                rand_bits = np.random.randint(2, size=rand_len)
-                self.randsrc = blocks.vector_source_b(rand_bits, True)
-                self.xor = blocks.xor_bb()
-                self.connect(self.randsrc,(self.xor,1))
-                self.connect(last, self.xor)
-                last = self.xor
 
         else:
 
